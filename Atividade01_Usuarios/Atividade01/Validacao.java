a59a8269e641297295aa28602f7a87b2089f3532
vitoriaalbuquerqueee <146150232+vitoriaalbuquerqueee@users.noreply.github.com>
2026-06-29 23:20:04 -0300

    update

--- Atividade01_Usuarios/Atividade01/Validacao.java
+++ Atividade01_Usuarios/Atividade01/Validacao.java
@@ -1,5 +1,15 @@
 package Atividade01;
 
 public class Validacao {
+    public static void validarTexto(String texto, String campo) {
+        if (texto == null || texto.trim().isEmpty()) {
+            throw new IllegalArgumentException("O campo " + campo + " não pode ser vazio!");
+        }
+    }
 
+    public static void validarEmail(String email) {
+        if (email == null || !email.contains("@")) {
+            throw new IllegalArgumentException("O e-mail digitado é inválido!");
+        }
+    }
 }
