7d9dc5d426c1cc031eddca835c14750eca34ed8c
vitoriaalbuquerqueee <146150232+vitoriaalbuquerqueee@users.noreply.github.com>
2026-06-29 23:18:26 -0300

    update

--- Atividade01_Usuarios/Atividade01/Administrador.java
+++ Atividade01_Usuarios/Atividade01/Administrador.java
@@ -1,5 +1,24 @@
 package Atividade01;
 
-public class Administrador {
+public class Administrador extends Usuario {
+    private String departamento;
+
+    public Administrador(String nome, String email, String departamento) {
+        super(nome, email);
+        Validacao.validarTexto(departamento, "Departamento");
+        this.departamento = departamento;
+    }
+
+    public String getDepartamento() { return departamento; }
+    public void setDepartamento(String departamento) { 
+        Validacao.validarTexto(departamento, "Departamento");
+        this.departamento = departamento; 
+    }
+
+    @Override
+    public void visualizar() {
+        super.visualizar();
+        System.out.println("Departamento: " + this.departamento);
+    }
 
 }
