1b146f67f893c0a684419f94f3d2fed944595b43
vitoriaalbuquerqueee <146150232+vitoriaalbuquerqueee@users.noreply.github.com>
2026-06-29 23:19:19 -0300

    Update

--- Atividade01_Usuarios/Atividade01/Usuario.java
+++ Atividade01_Usuarios/Atividade01/Usuario.java
@@ -1,5 +1,31 @@
 package Atividade01;
 
 public class Usuario {
+    private String nome;
+    private String email;
 
+    public Usuario(String nome, String email) {
+        Validacao.validarTexto(nome, "Nome");
+        Validacao.validarEmail(email);
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { 
+        Validacao.validarTexto(nome, "Nome");
+        this.nome = nome; 
+    }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { 
+        Validacao.validarEmail(email);
+        this.email = email; 
+    }
+
+    public void visualizar() {
+        System.out.println("Usuário: " + this.nome + " | Email: " + this.email);
+    }
 }
+
+
